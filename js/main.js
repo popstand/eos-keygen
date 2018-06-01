63baca20903ebc74e0f89a3ae57998b70e28d3fb
Eaven <eaven@popstand.com>
2018-06-01 11:47:33 -0700

    added server selection

--- js/main.js
+++ js/main.js
@@ -14,7 +14,23 @@ function toggleKeyInput () {
     }
     else {
         privateKeyInput.style.display = "none";
-        scatter.getIdentity();
+        if (typeof scatter === "undefined") {
+            var alert = `<div class="alert alert-danger" role="alert">
+                Scatter is not installed. Refresh page after installing.
+            </div>`
+            document.getElementById('alerts').innerHTML = alert;
+            return false;
+        }
+        else {
+            scatter.getIdentity().catch(err => {
+                if (err.type == "locked") {
+                    var alert = `<div class="alert alert-danger" role="alert">
+                        Please refresh page after unlocking Scatter.
+                    </div>`
+                    document.getElementById('alerts').innerHTML = alert;
+                }
+            });
+        }
     }
 }
 
@@ -32,25 +48,28 @@ function filterProds () {
 
 function getEos() {
     var method = document.querySelector('input[name="signing-method"]:checked').value;
+    var network = document.getElementById('network').value;
+    var ip = network.slice(network.lastIndexOf("/") + 1, network.lastIndexOf(":"));
+    var port = network.slice(network.lastIndexOf(":") + 1);
     if (method == "scatter") {
         var network = {
             blockchain: 'eos',
-            host: "13.71.191.137",
-            port: 8889,
+            host: ip,
+            port: port,
             chainId: "a628a5a6123d6ed60242560f23354c557f4a02826e223bb38aad79ddeb9afbca"
         }
         var config = {
             broadcast: true,
             sign: true,
             chainId: "a628a5a6123d6ed60242560f23354c557f4a02826e223bb38aad79ddeb9afbca"
-        }        
+        }
         return scatter.eos(network, Eos.Testnet, config);
     }
     else {
         var privateKey = document.getElementById('private-key').value;
         var config = {
             keyProvider: privateKey,
-            httpEndpoint: "http://13.71.191.137:8889",
+            httpEndpoint: network,
             broadcast: true,
             sign: true,
             chainId: "a628a5a6123d6ed60242560f23354c557f4a02826e223bb38aad79ddeb9afbca",
@@ -66,10 +85,11 @@ function getProducers() {
         json: true,
         scope: "eosio",
         code: "eosio",
-        table: "producers", 
+        table: "producers",
         limit: 200
     }
     var tbody = document.querySelector("#block-producers tbody");
+    tbody.innerHTML = '';
 
     return eos.getTableRows(params).then(resp => {
         var sorted = resp.rows.sort((a,b) => Number(a.total_votes) > Number(b.total_votes) ? -1:1);
@@ -108,6 +128,17 @@ function prettyNumber(num) {
     return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 }
 
+function refreshKeys() {
+    scatter.forgetIdentity()
+        .then(scatter.getIdentity)
+        .then(() => {
+            var alert = `<div class="alert alert-info" role="alert">
+                Keys refreshed.
+            </div>`
+            document.getElementById('alerts').innerHTML = alert;
+        });
+}
+
 function vote () {
     var eos = getEos();
 
@@ -133,12 +164,22 @@ function vote () {
         </div>`
         document.getElementById('alerts').innerHTML = alert;
         document.getElementById('private-key').value = "";
-        
+
         document.getElementById('vote').disabled = false;
     }).catch(err => {
         console.error(err);
+        if (typeof err == "string") {
+            err = JSON.parse(err);
+            var message = `Error: ${err.error.details[0].message}`;
+        }
+        else if (err.type == "account_missing")
+            var message = `Error: Key does not match account. Click here to use a <a href="#" onclick="refreshKeys()">different identity</a>`;
+        else if (err.message)
+            var message = `Error: Transaction failed. ${err.message}`;
+        else
+            var message = `Error: Transaction failed. ${err.type}. Try refreshing page.`;
         var alert = `<div class="alert alert-danger" role="alert">
-            Error: Transaction failed. ${err.message}
+            ${message}
         </div>`;
         document.getElementById('alerts').innerHTML = alert;
 
